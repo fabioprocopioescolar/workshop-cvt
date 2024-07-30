0d872728247239c89c77e961471c6783117ceb3a
Fábio Procópio <37914791+fabioprocopio@users.noreply.github.com>
2024-07-30 20:36:34 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//Compilador: https://www.online-cpp.com/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int nota1, nota2, media;
+  
+    cout << "Nota 1: ";
+    cin >> nota1;
+    cout << "Nota 2: ";
+    cin >> nota2;
+    
+    media = (nota1 + nota2) / 2;
+    
+    cout << "Média: " << media;
+    return 0;
+}
